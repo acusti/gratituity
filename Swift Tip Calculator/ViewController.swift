3a3a9e6e49097c73d78f200a12f8890585d69723
Andrew Patton <andrew@acusti.ca>
2014-12-02 00:00:57 -0500

    Initial Commit

---
+++ Swift Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Tip Calculator
+//
+//  Created by Andrew Patton on 2014-12-02.
+//  Copyright (c) 2014 acusti.ca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
